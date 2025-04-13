abd9b0313da253511aa9eaa32f38e6f99ad47aa2
ahmed <ahmedSamyAlashmony1@gmail.com>
2025-04-14 00:23:00 +0200

    adding pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './App.css'
+import Destination from './pages/Destination/Destination'
+import Crew from './pages/Crew/Crew'
+import Home from './pages/Home/Home'
+import Technology from './pages/Technology/Technology'
+
 
 function App() {
 
-  return (
-    <>
-      
-    </>
-  )
+  const router = createBrowserRouter([
+    { path: "/", element: <Home />, },
+    {path: "/destination",element: <Destination />,},
+    {path: "/crew",element: <Crew />,},
+    {path: "/technology",element: <Technology />,},
+  ])
+
+  return <>
+    <RouterProvider router={router} />
+  </>
+
 }
 
 export default App
